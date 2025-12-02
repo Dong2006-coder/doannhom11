e09da013132f1a59694a29fff19834b16ad3253f
Dong2006-coder <vuanhdong2006@gmail.com>
2025-12-03 01:19:25 +0700

    cap nhat code lan 2

---
+++ java.js
@@ -0,0 +1,19 @@
+let cartCount = 0;
+
+// Lấy phần hiển thị số lượng trong giỏ hàng
+const cartDisplay = document.getElementById("cart-count");
+
+// Lấy tất cả nút "Thêm vào giỏ"
+const addButtons = document.querySelectorAll(".add-btn");
+
+// Kiểm tra nếu tìm thấy giỏ hàng
+if (cartDisplay) {
+    addButtons.forEach(btn => {
+        btn.addEventListener("click", () => {
+            cartCount++;
+            cartDisplay.textContent = cartCount;
+        });
+    });
+} else {
+    console.log("Không tìm thấy #cart-count");
+}
